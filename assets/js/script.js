622fed14a1a8757e34e7a8ba74bec69a4b5dda67
Thomas McDonald <tommymc925@gmail.com>
2023-03-13 21:43:15 -0400

    homepage with functioning search bar

--- assets/js/script.js
+++
@@ -1,7 +0,0 @@
-// Nav bar functionallity
-const burgerIcon = document.querySelector(".burger-icon");
-const navbarMenu = document.querySelector(".navbar-menu");
-  
-burgerIcon.addEventListener("click", function() {
-  navbarMenu.classList.toggle("show");
-});
